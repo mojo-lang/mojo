776f5b803402afe9b2d454d98915b88a8a1b021b
Frankee.zhou <frankee.zhou@wayz.ai>
2022-03-26 16:33:37 +0800

    move UniqueStrings to core package

--- go/pkg/mojo/util/strings.go
+++ go/pkg/mojo/util/strings.go
@@ -1,23 +1,7 @@
 package util
 
-import (
-    "github.com/mojo-lang/core/go/pkg/mojo/core"
-)
+import "github.com/mojo-lang/core/go/pkg/mojo/core"
 
 func UniqueStringSlice(strings []string) []string {
-    keys := make(map[string]bool)
-    var list []string
-    for _, entry := range strings {
-        if _, value := keys[entry]; !value {
-            keys[entry] = true
-            list = append(list, entry)
-        }
-    }
-    return list
-}
-
-func UniqueStrings(strings *core.Strings) {
-    if strings != nil {
-        strings.Vals = UniqueStringSlice(strings.Vals)
-    }
+    return core.NewStringValues(strings...).Unique().Vals
 }
