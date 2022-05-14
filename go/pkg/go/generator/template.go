538cfcbb2a03019b8ecb957ee38db913fa04c072
Frankee.zhou <frankee.zhou@gmail.com>
2022-05-14 11:23:53 +0800

    update the json compiler and generator for the boxed type of map

--- go/pkg/go/generator/template.go
+++ go/pkg/go/generator/template.go
@@ -1,13 +1,13 @@
 package generator
 
 import (
-    "bytes"
-    _ "embed"
-    "strings"
-    "text/template"
+	"bytes"
+	_ "embed"
+	"strings"
+	"text/template"
 
-    "github.com/mojo-lang/core/go/pkg/mojo/core/strcase"
-    "github.com/pkg/errors"
+	"github.com/mojo-lang/core/go/pkg/mojo/core/strcase"
+	"github.com/pkg/errors"
 )
 
 //go:embed template/go/BOXED_ARRAY.ext.go.tmpl
@@ -16,10 +16,10 @@ var goArrayExtFile string
 //go:embed template/go/BOXED_ARRAY.json.go.tmpl
 var goArrayJsonFile string
 
-//go:embed template/go/BOXED_DICTIONARY.ext.go.tmpl
+//go:embed template/go/BOXED_MAP.ext.go.tmpl
 var goMapExtFile string
 
-//go:embed template/go/BOXED_DICTIONARY.json.go.tmpl
+//go:embed template/go/BOXED_MAP.json.go.tmpl
 var goMapJsonFile string
 
 //go:embed template/go/BOXED_UNION.ext.go.tmpl
@@ -47,32 +47,32 @@ var goModFile string
 // function map. Returns the output of the templates on success, returns an
 // error if templates failed to execute.
 func ApplyTemplate(name string, tmpl string, executor interface{}, funcMap template.FuncMap) (string, error) {
-    codeTemplate := template.Must(template.New(name).Funcs(funcMap).Parse(tmpl))
+	codeTemplate := template.Must(template.New(name).Funcs(funcMap).Parse(tmpl))
 
-    code := bytes.NewBuffer(nil)
-    err := codeTemplate.Execute(code, executor)
-    if err != nil {
-        return "", errors.Wrapf(err, "attempting to execute templates %q", name)
-    }
-    return code.String(), nil
+	code := bytes.NewBuffer(nil)
+	err := codeTemplate.Execute(code, executor)
+	if err != nil {
+		return "", errors.Wrapf(err, "attempting to execute templates %q", name)
+	}
+	return code.String(), nil
 }
 
 func CompactFullName(name string) string {
-    return strings.ReplaceAll(name, "_", "")
+	return strings.ReplaceAll(name, "_", "")
 }
 
 // FuncMap contains a series of utility functions to be passed into
 // templates and used within those templates.
 var FuncMap = template.FuncMap{
-    "ToLower":          strings.ToLower,
-    "ToUpper":          strings.ToUpper,
-    "GoName":           strcase.ToCamel,
-    "ToSnake":          strcase.ToSnake,
-    "ToScreamingSnake": strcase.ToScreamingSnake,
-    "ToKebab":          strcase.ToKebab,
-    "ToCamel":          strcase.ToCamel,
-    "ToLowerCamel":     strcase.ToLowerCamel,
-    "CompactFullName":  CompactFullName,
-    "IsMojoPackage":    func(pkg string) bool { return strings.HasPrefix(pkg, "mojo.") },
-    "IsPackage":        func(srcPkg string, targetPkg string) bool { return strings.HasPrefix(srcPkg, targetPkg) },
+	"ToLower":          strings.ToLower,
+	"ToUpper":          strings.ToUpper,
+	"GoName":           strcase.ToCamel,
+	"ToSnake":          strcase.ToSnake,
+	"ToScreamingSnake": strcase.ToScreamingSnake,
+	"ToKebab":          strcase.ToKebab,
+	"ToCamel":          strcase.ToCamel,
+	"ToLowerCamel":     strcase.ToLowerCamel,
+	"CompactFullName":  CompactFullName,
+	"IsMojoPackage":    func(pkg string) bool { return strings.HasPrefix(pkg, "mojo.") },
+	"IsPackage":        func(srcPkg string, targetPkg string) bool { return strings.HasPrefix(srcPkg, targetPkg) },
 }
--- go/pkg/protobuf/precompiler/map.go
+++ go/pkg/protobuf/precompiler/map.go
@@ -1,46 +1,50 @@
 package precompiler
 
 import (
-    "errors"
-    "github.com/mojo-lang/core/go/pkg/mojo/core"
-    "github.com/mojo-lang/core/go/pkg/mojo/core/strcase"
-    "github.com/mojo-lang/lang/go/pkg/mojo/lang"
-    "github.com/mojo-lang/mojo/go/pkg/mojo/compiler/transformer"
-    "github.com/mojo-lang/mojo/go/pkg/mojo/context"
-    "strings"
+	"errors"
+	"github.com/mojo-lang/core/go/pkg/mojo/core"
+	"github.com/mojo-lang/core/go/pkg/mojo/core/strcase"
+	"github.com/mojo-lang/lang/go/pkg/mojo/lang"
+	"github.com/mojo-lang/mojo/go/pkg/mojo/compiler/transformer"
+	"github.com/mojo-lang/mojo/go/pkg/mojo/context"
+	"strings"
 )
 
 func CompileMapToStruct(ctx context.Context, t *lang.NominalType) (*lang.StructDecl, error) {
-    if t.GetFullName() != core.MapTypeFullName {
-        return nil, errors.New("")
-    }
+	if t.GetFullName() != core.MapTypeFullName {
+		return nil, errors.New("")
+	}
 
-    if len(t.GenericArguments) != 2 {
-        return nil, errors.New("")
-    }
+	if len(t.GenericArguments) != 2 {
+		return nil, errors.New("")
+	}
 
-    t.Attributes = lang.SetIntegerAttribute(t.Attributes, core.NumberAttributeName, 1)
+	t.Attributes = lang.SetIntegerAttribute(t.Attributes, core.NumberAttributeName, 1)
 
-    s := &lang.StructDecl{}
-    s.Type = &lang.StructType{
-        Fields: []*lang.ValueDecl{{
-            Name: "vals",
-            Type: t,
-        }},
-    }
+	s := &lang.StructDecl{}
+	s.Type = &lang.StructType{
+		Fields: []*lang.ValueDecl{{
+			Name: "vals",
+			Type: t,
+		}},
+	}
 
-    val := t.GenericArguments[0]
+	if name, _ := lang.GetStringAttribute(t.Attributes, lang.OriginalTypeAliasName); len(name) > 0 {
+		if strings.Contains(name, "<") {
+			nominalType, _ := lang.ParseNominalTypeFullName(name)
+			s.Name = transformer.GenericTypeNamer{}.Name(nominalType)
+		} else {
+			s.Name = name
+		}
+	} else {
+		val := t.GenericArguments[1]
+		//TODO need precompile firstly
+		if transformer.IsSingular(val.Name) {
+			s.Name = transformer.Plural(strcase.ToCamel(val.Name))
+		} else {
+			s.Name = val.Name + "Map"
+		}
+	}
 
-    if name, _ := lang.GetStringAttribute(t.Attributes, lang.OriginalTypeAliasName); len(name) > 0 {
-        if strings.Contains(name, "<") {
-            nominalType, _ := lang.ParseNominalTypeFullName(name)
-            s.Name = transformer.GenericTypeNamer{}.Name(nominalType)
-        } else {
-            s.Name = name
-        }
-    } else {
-        s.Name = transformer.Plural(strcase.ToCamel(val.Name))
-    }
-
-    return s, nil
+	return s, nil
 }
