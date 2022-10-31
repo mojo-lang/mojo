219d53cac3b946286231f94d3c92c4e6835affd1
Frankee.zhou <frankee.zhou@gmail.com>
2022-10-31 18:02:00 +0800

    fix the bug for the irregularCaseRuleCompiler

--- go/pkg/mojo/compiler/irregular_case_rule_compiler.go
+++ go/pkg/mojo/compiler/irregular_case_rule_compiler.go
@@ -1,67 +1,71 @@
 package compiler
 
 import (
-    "github.com/mojo-lang/core/go/pkg/logs"
-    "github.com/mojo-lang/core/go/pkg/mojo/core"
-    "github.com/mojo-lang/lang/go/pkg/mojo/lang"
-    "github.com/mojo-lang/mojo/go/pkg/mojo/context"
-    "github.com/mojo-lang/mojo/go/pkg/mojo/plugin"
+	"github.com/mojo-lang/core/go/pkg/logs"
+	"github.com/mojo-lang/core/go/pkg/mojo/core"
+	"github.com/mojo-lang/lang/go/pkg/mojo/lang"
+
+	"github.com/mojo-lang/mojo/go/pkg/mojo/context"
+	"github.com/mojo-lang/mojo/go/pkg/mojo/plugin"
 )
 
 const irregularCaseRuleName = "compiler.irregular-case-rule"
 
 func init() {
-    plugin.RegisterPlugin(NewIrregularCaseRuleCompiler(nil))
+	plugin.RegisterPlugin(NewIrregularCaseRuleCompiler(nil))
 }
 
 type IrregularCaseRuleCompiler struct {
-    plugin.BasicPlugin
+	plugin.BasicPlugin
 }
 
 func NewIrregularCaseRuleCompiler(options core.Options) *IrregularCaseRuleCompiler {
-    return &IrregularCaseRuleCompiler{
-        BasicPlugin: plugin.BasicPlugin{
-            Name:          irregularCaseRuleName,
-            Group:         "compiler",
-            GroupPriority: 10,
-            Priority:      2,
-            Creator: func(options core.Options) plugin.Plugin {
-                return NewIrregularCaseRuleCompiler(options)
-            },
-            MarkedPackages: make(map[string]bool),
-        },
-    }
+	return &IrregularCaseRuleCompiler{
+		BasicPlugin: plugin.BasicPlugin{
+			Name:          irregularCaseRuleName,
+			Group:         "compiler",
+			GroupPriority: 10,
+			Priority:      2,
+			Creator: func(options core.Options) plugin.Plugin {
+				return NewIrregularCaseRuleCompiler(options)
+			},
+			MarkedPackages: make(map[string]bool),
+		},
+	}
 }
 
 func (c *IrregularCaseRuleCompiler) CompileSourceFile(ctx context.Context, sourceFile *lang.SourceFile) error {
-    pkg := context.Package(ctx)
-    pkgFullName := pkg.GetFullName()
+	pkg := context.Package(ctx)
+	pkgFullName := pkg.GetFullName()
 
-    if !c.MarkedPackages[pkgFullName] {
-        c.MarkedPackages[pkgFullName] = true
-        logs.Infow("enter the plugin", "plugin", c.Name, "method", "CompilePackage", "pkg", pkgFullName)
-    }
+	if !c.MarkedPackages[pkgFullName] {
+		c.MarkedPackages[pkgFullName] = true
+		logs.Infow("enter the plugin", "plugin", c.Name, "method", "CompilePackage", "pkg", pkgFullName)
+	}
 
-    for _, statement := range sourceFile.Statements {
-        if decl := statement.GetDeclaration(); decl != nil {
-            return c.ApplyAttribute(ctx, lang.GetAttributes(decl).GetAttribute(core.IrregularCaseRuleAttributeName))
-        }
-    }
-    return nil
+	for _, statement := range sourceFile.Statements {
+		if decl := statement.GetDeclaration(); decl != nil {
+			err := c.ApplyAttribute(ctx, lang.GetAttributes(decl).GetAttribute(core.IrregularCaseRuleAttributeName))
+			if err != nil {
+				return err
+			}
+		}
+	}
+	return nil
 }
 
 func (c *IrregularCaseRuleCompiler) ApplyAttribute(ctx context.Context, attribute *lang.Attribute) (err error) {
-    if attribute != nil && len(attribute.Arguments) > 0 {
-        if len(attribute.Arguments) == 2 {
-            rule, replacement := "", ""
-            if rule, err = attribute.Arguments[0].GetString(); err != nil {
-                return err
-            }
-            if replacement, err = attribute.Arguments[1].GetString(); err != nil {
-                return err
-            }
-            err = core.ApplyIrregularCaseRuleAttribute(&core.Regex{Expression: rule}, replacement)
-        }
-    }
-    return
+	if attribute != nil && len(attribute.Arguments) > 0 {
+		if len(attribute.Arguments) == 2 {
+			rule, replacement := "", ""
+			if rule, err = attribute.Arguments[0].GetString(); err != nil {
+				return err
+			}
+			if replacement, err = attribute.Arguments[1].GetString(); err != nil {
+				return err
+			}
+			err = core.ApplyIrregularCaseRuleAttribute(&core.Regex{Expression: rule}, replacement)
+		}
+	}
+	return
 }
